a0b4a2a66f73b8d7967c4a23da94e4842c3bca28
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-09-14 22:10:00 +0900

    Added hello-world api

--- spring-boot/src/main/java/com/example/jchankchan/DemoApplication.java
+++ spring-boot/src/main/java/com/example/jchankchan/DemoApplication.java
@@ -2,16 +2,22 @@ package com.example.jchankchan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String index(){
-		return "Hello JchanKchan!";
+	@GetMapping(path = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+	String hello(@RequestParam(name = "message", required = false) String message){
+		if (message == null || message.isEmpty() || message.isBlank()) {
+			return "Hello JchanKchan!";
+		} else {
+			return "Hello " + message + "!";
+		}
 	}
 
 	public static void main(String[] args) {
